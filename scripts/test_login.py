af6a248a5f83620b598a3b4444669862348c0dd5
evanli01 <595978837@qq.com>
2021-02-14 23:11:44 +0800

    first commit

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,9 +1,9 @@
 class TestLogin(object):
     def test_login_001(self):
-        assert 1
+        assert 0
 
     def test_login_002(self):
-        assert 1
+        assert 0
 
     def test_login_003(self):
         assert 1
